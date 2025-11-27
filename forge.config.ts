8abd81aa170a7e62962b098a5be841fefc8eee5b
Gayuh Ridho <gayuhridho369@gmail.com>
2025-11-27 17:37:20 +0700

    feat: publisher and github release

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import path from 'node:path'
 
 import { FuseV1Options, FuseVersion } from '@electron/fuses'
 import { MakerDeb } from '@electron-forge/maker-deb'
+import { MakerDMG } from '@electron-forge/maker-dmg'
 import { MakerRpm } from '@electron-forge/maker-rpm'
 import { MakerSquirrel } from '@electron-forge/maker-squirrel'
 import { MakerZIP } from '@electron-forge/maker-zip'
@@ -18,10 +19,12 @@ const config: ForgeConfig = {
       unpackDir: '{better-sqlite3}',
     },
   },
+
   rebuildConfig: {
     onlyModules: ['better-sqlite3'],
     force: true,
   },
+
   hooks: {
     async packageAfterCopy(_forgeConfig, buildPath) {
       const requiredNativePackages = [
@@ -47,19 +50,65 @@ const config: ForgeConfig = {
       )
     },
   },
+
   makers: [
-    new MakerSquirrel({}),
+    // Windows - Squirrel Installer
+    new MakerSquirrel({
+      name: 'ElectronBasic',
+      authors: 'Gayuh Ridho',
+      description: 'Electron Basic Application',
+    }),
+
+    // Windows - Portable ZIP
+    new MakerZIP({}, ['win32']),
+
+    // macOS - DMG Installer (Tambahan dari config lama Anda)
+    new MakerDMG({
+      format: 'ULFO',
+      name: 'ElectronBasic',
+    }),
+
+    // macOS - ZIP (untuk auto-update)
     new MakerZIP({}, ['darwin']),
-    new MakerRpm({}),
-    new MakerDeb({}),
+
+    // Linux - Debian/Ubuntu
+    new MakerDeb({
+      options: {
+        maintainer: 'Gayuh Ridho <gayuhridho369@gmail.com>',
+        homepage: 'https://github.com/gayuhridho369/electron-basic',
+        categories: ['Utility', 'Development'],
+        section: 'utils',
+      },
+    }),
+
+    // Linux - Fedora/RHEL
+    new MakerRpm({
+      options: {
+        homepage: 'https://github.com/gayuhridho369/electron-basic',
+        categories: ['Utility', 'Development'],
+      },
+    }),
   ],
+
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'gayuhridho369',
+          name: 'electron-basic',
+        },
+        prerelease: false, // Changed from true - set true jika mau pre-release
+        draft: true, // Release sebagai draft dulu, bisa edit sebelum publish
+        generateReleaseNotes: true, // Auto generate dari commits
+      },
+    },
+  ],
+
   plugins: [
     new VitePlugin({
-      // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-      // If you are familiar with Vite configuration, it will look really familiar.
       build: [
         {
-          // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
           entry: 'src/main.ts',
           config: 'vite.main.config.mts',
           target: 'main',
@@ -77,9 +126,9 @@ const config: ForgeConfig = {
         },
       ],
     }),
+
     new AutoUnpackNativesPlugin({}),
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
+
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
